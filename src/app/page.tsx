8fe0d1e040b4e43db501fbdf70627e27269bde77
Thomas von Stetten <tvstetten@tvs-soft.de>
2024-03-06 17:16:18 +0100

    Using SWR-package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { getData } from '@/actions/todoActions'
 import Todos from '@/components/Todos'
+import { todoType } from '@/types/todo'
+import useSWR from 'swr'
 
 export default async function Home() {
   // load server-side
-  const data = await getData()
-  console.log('Home->data', data)
-  return <Todos todos={data} />
+  // const dataInfo = await useTodoData()
+  // console.log('Home->data', dataInfo)
+
+  return <Todos />
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,43 +1,63 @@
 'use client'
-import { FC, useState } from 'react'
-import { todoType } from '@/types/todo'
+
 import Todo from './Todo'
 import AddTodo from './AddTodo'
-import { addTodo, deleteTodo, editTodo, toggleTodo } from '@/actions/todoActions'
+import { addTodo, deleteTodo, editTodo, getData, toggleTodo } from '@/actions/todoActions'
+import useSWR from 'swr'
 
-interface Props {
-  todos: todoType[]
+const SWR_KEY = 'api/todos'
+
+export const useTodoData = () => {
+  return useSWR(SWR_KEY, () => getData())
 }
 
-const Todos: FC<Props> = ({ todos }) => {
+const Todos = () => {
   // State to manage the list of todo items
-  const [todoItems, setTodoItems] = useState<todoType[]>(todos)
+  const { data: todoItems, mutate, error, isValidating, isLoading } = useTodoData()
+  console.log('Todos->todosInfo', todoItems, error, isValidating, isLoading)
+
+  if (isLoading || todoItems === undefined) return <div>Loading...</div>
+  // if (isValidating) return <div>Validating...</div>
 
   // Function to create a new todo item
   const createTodo = (text: string) => {
     const id = (todoItems.at(-1)?.id || 0) + 1
     addTodo(id, text)
-    setTodoItems(prev => [...prev, { id: id, text, done: false }])
+    // setTodoItems(prev => [...prev, { id: id, text, done: false }])
+    // todoItems.push({ id, text, done: false })
+    mutate(todoItems, { revalidate: true })
   }
 
   // Function to change the text of a todo item
   const changeTodoText = (id: number, text: string) => {
-    setTodoItems(prev => prev.map(todo => (todo.id === id ? { ...todo, text } : todo)))
+    // setTodoItems(prev => prev.map(todo => (todo.id === id ? { ...todo, text } : todo)))
     editTodo(id, text)
+    const i = todoItems.findIndex(todo => todo.id === id)
+    if (i >= 0) todoItems[i].text = todoItems[i].text
+    mutate(todoItems, { revalidate: false })
   }
 
   // Function to toggle the "done" status of a todo item
   const toggleIsTodoDone = (id: number) => {
-    setTodoItems(prev =>
-      prev.map(todo => (todo.id === id ? { ...todo, done: !todo.done } : todo)),
-    )
+    // setTodoItems(prev =>
+    //   prev.map(todo => (todo.id === id ? { ...todo, done: !todo.done } : todo)),
+    // )
     toggleTodo(id)
+    // ------------
+    // Update only the changed property
+    const i = todoItems.findIndex(todo => todo.id === id)
+    if (i >= 0) todoItems[i].done = !todoItems[i].done
+    mutate(todoItems, { revalidate: false })
   }
 
   // Function to delete a todo item
   const deleteTodoItem = (id: number) => {
-    setTodoItems(prev => prev.filter(todo => todo.id !== id))
+    // setTodoItems(prev => prev.filter(todo => todo.id !== id))
     deleteTodo(id)
+    mutate(
+      todoItems.filter(todo => todo.id !== id),
+      { revalidate: false },
+    )
   }
 
   // Rendering the Todo List component
@@ -46,6 +66,7 @@ const Todos: FC<Props> = ({ todos }) => {
       <div className="text-5xl font-medium">To-do app</div>
       <div className="w-full flex flex-col mt-8 gap-2">
         {/* Mapping through todoItems and rendering Todo component for each */}
+        {console.log('build')}
         {todoItems.map(todo => (
           <Todo
             key={todo.id}
